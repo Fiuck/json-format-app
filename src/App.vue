8173d5e97a0c6da8de82d4acd4db2a0c581cd052
zhaowx <643948989@qq.com>
2022-09-01 21:21:35 +0800

    add monaco-editor

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,44 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
-</script>
-
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-
-    <Greet />
-  </div>
+  <div ref="main" style="width: 100%; height: 500px"></div>
 </template>
 
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+<script lang="ts">
+import * as monaco from "monaco-editor";
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-</style>
+export default {
+  data() {
+    return {
+      monacoEditor: null,
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      // 使用 - 创建 monacoEditor 对象
+      this.monacoEditor = monaco.editor.create(this.$refs.main, {
+        theme: "vs", // 主题
+        value: "// 输入json以格式化\n", // 默认显示的值
+        language: "json",
+        folding: true, // 是否折叠
+        foldingHighlight: true, // 折叠等高线
+        foldingStrategy: "indentation", // 折叠方式  auto | indentation
+        showFoldingControls: "always", // 是否一直显示折叠 always | mouseover
+        disableLayerHinting: true, // 等宽优化
+        emptySelectionClipboard: false, // 空选择剪切板
+        selectionClipboard: false, // 选择剪切板
+        automaticLayout: true, // 自动布局
+        codeLens: false, // 代码镜头
+        scrollBeyondLastLine: false, // 滚动完最后一行后再滚动一屏幕
+        colorDecorators: true, // 颜色装饰器
+        accessibilitySupport: "off", // 辅助功能支持  "auto" | "off" | "on"
+        lineNumbers: "on", // 行号 取值： "on" | "off" | "relative" | "interval" | function
+        lineNumbersMinChars: 5, // 行号最小字符   number
+        enableSplitViewResizing: false,
+        readOnly: false, //是否只读  取值 true | false
+      });
+    },
+  },
+};
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from "vue";
-import "./style.css";
+// import "./style.css";
 import App from "./App.vue";
-
-import { invoke } from '@tauri-apps/api'
 
 createApp(App).mount("#app");
 
-invoke('greet', {name: 'hello world'}).then(response => console.log(response));
+
